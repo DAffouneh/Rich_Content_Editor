685c674258591de3d8870fd60a3218e7ed7085a7
Dania Aff <daffouneh@asaltech.com>
2020-11-17 16:18:00 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
+import YoutubeIcon from './YouTube.svg';
+import GifIcon from './Gif.svg';
 import './App.css';
+import classes from './UI/Footer/Footer.module.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <div className={classes.footer}>
+        <img className={classes.youtube_icon} src={YoutubeIcon}></img>
+        <img src={GifIcon}></img>
+
+    </div>
     </div>
   );
 }
---
+++ src/UI/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from './Footer.module.css';
+const Footer = () => (
+  <div className={classes.footer}>
+    <button className={classes.youtube_icon}></button>
+    <button></button>
+  </div>
+  
+);
+
+export default Footer;
