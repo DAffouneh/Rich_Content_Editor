4b93c9fc4663fbeb891b8a7e7254bf6b576004fd
DAffouneh <71401826+DAffouneh@users.noreply.github.com>
2020-11-22 23:16:34 +0200

    selected item does not work well

--- src/App.js
+++ src/App.js
@@ -2,40 +2,41 @@ import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
 import YoutubeModal from "./UI/YoutubeModal/YoutubeModal";
-import GifModal from './UI/GifModal/GifModal';
+import GifModal from "./UI/GifModal/GifModal";
 import YoutubeSearchBar from "./components/Youtube/YoutubeSearchBar/YoutubeSearchBar";
 import VideoList from "./components/Youtube/VideoList/VideoList";
 import Spinner from "./UI/Spinner/Spinner";
 import YoutubeIcon from "./YouTube.svg";
 import GifIcon from "./Gif.svg";
 import classes from "./App.module.css";
-import GifList from './components/Gif/GifList/GifList';
+import GifList from "./components/Gif/GifList/GifList";
 import Search from "./search.svg";
 import Cancel from "./cancel.svg";
 import VideoSelectedList from "./components/Youtube/VideoSelectedList/VideoSelectedList";
 import GifSelectedList from "./components/Gif/GifSelectedList/GifSelectedList";
 import copyright from "./copyright.svg";
-import Input from 'react-delay-input';
+import Input from "react-delay-input";
 
 var GphApiClient = require("giphy-js-sdk-core");
 const App = () => {
   const KEY = "AIzaSyC0lAY7iOLOZypsCZsp_yTU41BT0tRZ9Ac";
   const [pageToken, setPageToken] = useState("CAoQAA");
-  const [paginate,setPaginate]=useState(0);
+  const [paginate, setPaginate] = useState(0);
   const [videos, setVideos] = useState([]);
-  const[selectedVideos, setSelectedVideos]=useState([]);
+  const [selectedVideos, setSelectedVideos] = useState([]);
   const [gifs, setGifs] = useState([]);
   const [term, setTerm] = useState("");
   const [showYoutubeModal, setShowYoutubeModal] = useState(false);
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [selectedGif,setSelectedGif]=useState(null);
-  const[selctedGifs]=useState([]);
-  const [showGifModal,setshowGifModal]=useState(false);
+  const [selectedGif, setSelectedGif] = useState(null);
+  const [selctedGifs] = useState([]);
+  const [showGifModal, setshowGifModal] = useState(false);
   const [showVideo, setShowVideo] = useState(false);
-  const [showGif,setShowGif]= useState(false);
-  const[gifTermSearch,setGifTermSearch]=useState("");
-  const[error,setError]=useState("");
-  const giphy= GphApiClient("6KcE3OAJhPm3KrUtKUait8PaBac1v3Fq");
+  const [showGif, setShowGif] = useState(false);
+  const [gifTermSearch, setGifTermSearch] = useState("");
+  const [error, setError] = useState("");
+  const [flag, setFlag] = useState(null);
+  const giphy = GphApiClient("6KcE3OAJhPm3KrUtKUait8PaBac1v3Fq");
 
   useEffect(() => {
     searchHandler(term);
@@ -50,20 +51,16 @@ const App = () => {
         `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&order=viewCount&pageToken=${pageToken}&q=${termFromSearchBar}&type=video&key=${KEY}`
       )
       .then((res) => {
-        if(res.data.items.length===0)
-        {
+        if (res.data.items.length === 0) {
           setError("NO RESULTS");
-        }
-        else {
+        } else {
           setPageToken(res.data.nextPageToken);
           setVideos([...res.data.items]);
-          setError("")
-
+          setError("");
         }
-        
-      }).catch((err)=> 
-      {
-        setError("CONNECTION ERROR!")
+      })
+      .catch((err) => {
+        setError("CONNECTION ERROR!");
       });
   };
 
@@ -75,11 +72,8 @@ const App = () => {
       .then((res) => {
         setPageToken(res.data.nextPageToken);
         setVideos([...videos, ...res.data.items]);
-        
       })
-      .catch((err)=>{
-
-      });
+      .catch((err) => {});
   };
   const loadFeed = () => {
     giphy.trending();
@@ -115,28 +109,26 @@ const App = () => {
   };
   const handleVideoSelect = (video) => {
     setSelectedVideo(video);
-    let shownVideos = selectedVideos
+    let shownVideos = selectedVideos;
     shownVideos.push(video);
-    setSelectedVideos(shownVideos)
+    setSelectedVideos(shownVideos);
 
+    setFlag("video");
     setShowVideo(true);
   };
 
-  const handleGifSelect =(gif)=> 
-  {
-setSelectedGif(gif);
-let shownGifs = selectedVideos
-shownGifs.push(gif);
-setSelectedVideos(shownGifs);
-setShowGif(true)
-
-  }
-
-
+  const handleGifSelect = (gif) => {
+    setSelectedGif(gif);
+    let shownGifs = selectedVideos;
+    shownGifs.push(gif);
+    setSelectedVideos(shownGifs);
+    setShowGif(true);
+    setFlag("gif");
+  };
 
   const onSearchChange = (event) => {
-   setGifTermSearch(event.target.value);
-   search(event.target.value);
+    setGifTermSearch(event.target.value);
+    search(event.target.value);
   };
 
   const loadMoreGifs = () => {
@@ -148,9 +140,9 @@ setShowGif(true)
     setshowGifModal(!showGifModal);
   };
   const deleteSearchValue = () => {
-    setGifTermSearch("")  
-      loadFeed();
-    };
+    setGifTermSearch("");
+    loadFeed();
+  };
 
   let cancel = null;
   if (gifTermSearch !== "" && gifTermSearch !== "Search GIPHY...") {
@@ -163,132 +155,120 @@ setShowGif(true)
     );
   }
 
-
-
-
   const spinner = <Spinner></Spinner>;
   let display = null;
-  let gifdisplay=null;
-
-  if(showGif || showVideo)
-  {
-    gifdisplay= <div className={classes.slectedItems}>
-      <p>Selected Items </p>
-            <VideoSelectedList videos={selectedVideos}s />
-            {/* <GifSelectedList gifs={selctedGifs} /> */}
-
-
-    </div>
-
+  let gifdisplay = null;
+  if (showGif || showVideo) {
+    gifdisplay = (
+      <div className={classes.slectedItems}>
+        <p>Selected Items </p>
+        <VideoSelectedList items={selectedVideos} flag={flag} />
+        {/* <GifSelectedList gifs={selctedGifs} /> */}
+      </div>
+    );
   }
 
-  if(showYoutubeModal){
-    if(error !== "")
-    {
-      display=<div className={classes.youtubeModalContainer}>
-      <YoutubeModal
-        show={showYoutubeModal}
-        modalClosed={modalremovalHandler}
-        modalExitClosed={modalremovalHandler}
-      >
-        <YoutubeSearchBar
-          clickSearchHandeler={searchHandler}
-          term={term}
-        ></YoutubeSearchBar>
-        <div className={classes.error}>{error}</div>
-        </YoutubeModal>
-        </div>
-    }
-    else{
+  if (showYoutubeModal) {
+    if (error !== "") {
       display = (
         <div className={classes.youtubeModalContainer}>
-        <YoutubeModal
-          show={showYoutubeModal}
+          <YoutubeModal
+            show={showYoutubeModal}
+            modalClosed={modalremovalHandler}
+            modalExitClosed={modalremovalHandler}
+          >
+            <YoutubeSearchBar
+              clickSearchHandeler={searchHandler}
+              term={term}
+            ></YoutubeSearchBar>
+            <div className={classes.error}>{error}</div>
+          </YoutubeModal>
+        </div>
+      );
+    } else {
+      display = (
+        <div className={classes.youtubeModalContainer}>
+          <YoutubeModal
+            show={showYoutubeModal}
+            modalClosed={modalremovalHandler}
+            modalExit={modalremovalHandler}
+          >
+            <YoutubeSearchBar
+              clickSearchHandeler={searchHandler}
+              term={term}
+            ></YoutubeSearchBar>
+
+            <InfiniteScroll
+              dataLength={videos.length}
+              next={loadMore}
+              height={"280px"}
+              hasMore={true}
+              loader={spinner}
+              scrollThreshold={0.9}
+              className={classes.Scroll}
+            >
+              <VideoList
+                videos={videos}
+                handleVideoSelect={handleVideoSelect}
+              />
+            </InfiniteScroll>
+          </YoutubeModal>
+        </div>
+      );
+    }
+  } else if (showGifModal) {
+    display = (
+      <div className={classes.gifmodalContainer}>
+        <GifModal
+          show={showGifModal}
           modalClosed={modalremovalHandler}
           modalExit={modalremovalHandler}
         >
-          <YoutubeSearchBar
-            clickSearchHandeler={searchHandler}
-            term={term}
-          ></YoutubeSearchBar>
-  
+          <img src={Search} className={classes.SearchImg}></img>
+          <Input
+            className={classes.Input}
+            type="text"
+            placeholder={"Search GIPHY..."}
+            value={gifTermSearch}
+            minLength={1}
+            delayTimeout={300}
+            onChange={onSearchChange}
+          ></Input>
+          {cancel}
+          <div className={classes.copyRight}>
+            <img src={copyright} className={classes.CopyRightImg}></img>
+            <p className={classes.copyText}>2020 GIPHY , Inc.</p>
+          </div>
           <InfiniteScroll
-            dataLength={videos.length}
-            next={loadMore}
-            height={"280px"}
+            dataLength={gifs.length}
+            next={loadMoreGifs}
+            height={"200px"}
             hasMore={true}
             loader={spinner}
-            scrollThreshold={0.9}
+            scrollThreshold={0.6}
             className={classes.Scroll}
           >
-            <VideoList videos={videos} handleVideoSelect={handleVideoSelect} />
+            <GifList gifs={[...gifs]} handleGifSelect={handleGifSelect} />
           </InfiniteScroll>
-        </YoutubeModal>
-        </div>
-
-      );
-     }
-    
-  } else if(showGifModal)
-  {
-    display = (
-              <div className={classes.gifmodalContainer}>
-<GifModal
-        show={showGifModal}
-        modalClosed={modalremovalHandler}
-        modalExit={modalremovalHandler}
-
-       
-      >
-        <img src={Search} className={classes.SearchImg}></img>
-       <Input
-          className={classes.Input}
-          type="text"
-          placeholder={"Search GIPHY..."}
-          value={gifTermSearch}
-          minLength={1}
-          delayTimeout={300}
-          onChange={onSearchChange}
-        ></Input>
-        {cancel}
-        <div className={classes.copyRight}><img src={copyright}
-        className={classes.CopyRightImg}
-        ></img>
-        <p className={classes.copyText}>2020 GIPHY , Inc.</p></div>
-        <InfiniteScroll
-          dataLength={gifs.length}
-          next={loadMoreGifs}
-          height={"200px"}
-          hasMore={true}
-          loader={spinner}
-          scrollThreshold={0.6}
-          className={classes.Scroll}
-        >
-          <GifList gifs={[...gifs]} handleGifSelect={handleGifSelect} />
-        </InfiniteScroll>
-      </GifModal>
-        </div>
-      
+        </GifModal>
+      </div>
     );
-
   }
 
   return (
     <div>
       {gifdisplay}
       <div className={classes.footer}>
-          {display}
-          <img
-            className={classes.youtube_icon}
-            src={YoutubeIcon}
-            onClick={ModalShow}
-            alt="Youtube"
-          ></img>
-          <img src={GifIcon}
-          onClick={gifModalShow}
-          ></img>
-        </div>
+        {display}
+        <img
+          className={classes.youtube_icon}
+          src={YoutubeIcon}
+          onClick={ModalShow}
+          alt="Youtube"
+        ></img>
+        <img src={GifIcon} onClick={gifModalShow}></img>
       </div>
+    </div>
   );
 };
 
--- src/components/Youtube/VideoSelectedList/VideoSelectedList.js
+++ src/components/Youtube/VideoSelectedList/VideoSelectedList.js
@@ -1,26 +1,44 @@
 import React from "react";
 import VideoDetail from "../VideoDetail/VideoDetail";
-import GifDetail from "..//GifDetails/GifDetails";
+import GifDetail from "../../Gif/GifDetails/GifDetails";
 import classes from "./VideoSelectedList.module.css";
 
-const VideoSelectedList = ({ item, flag }) => {
-  const videoSelected = item.map((item) => {
-    
-    return (
-      flag === 'video' ?
-      <div key={item.id.videoId}>
-        <VideoDetail video={item} />
-      </div> : flag === 'gif' &&
-      
-      <div key={item.id}>
-      <GifDetail gif={item} />
-    </div> 
+const VideoSelectedList = ({ items, flag }) => {
+  let GifSelected = null;
+  if (flag === "gif") {
+    GifSelected = items.map((item) => {
+      return (
+        <div key={item.id}>
+          <GifDetail gif={item} />
+        </div>
+      );
+    });
+  } else if (flag === "video") {
+    GifSelected = items.map((item) => {
+      return (
+        <div key={item.id.videoId}>
+          <VideoDetail video={item} />
+        </div>
+      );
+    });
+  }
 
-    );
-  });
+  // const videoSelected = items.map((item) => {
+  //   return flag === "video" ? (
+  //     <div key={item.id.videoId}>
+  //       <VideoDetail video={item} />
+  //     </div>
+  //   ) : (
+  //     flag === "gif" && (
+  //       <div key={item.id}>
+  //         <GifDetail gif={item} />
+  //       </div>
+  //     )
+  //   );
+  // });
   return (
     <div className={classes.OuterDiv}>
-      <div className={classes.InnerDiv}>{videoSelected}</div>
+      <div className={classes.InnerDiv}>{GifSelected}</div>
     </div>
   );
 };
